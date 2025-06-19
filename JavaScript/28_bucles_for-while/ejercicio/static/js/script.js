14880cbcf3f9d0d9021e88cd3f52d72894e9e0a1
AlexanderRiquelme <alexander.riquelme676@gmail.com>
2025-06-19 09:30:16 -0400

    js

--- JavaScript/28_bucles_for-while/ejercicio/static/js/script.js
+++ JavaScript/28_bucles_for-while/ejercicio/static/js/script.js
@@ -1,8 +1,23 @@
 function contar() {
-    let N = parseInt(prompt("ingre hasta que número desea contar")), l = [];
-    while (let i = 1; i <= 5 ; i++){
+    let n = parseInt(prompt("ingrese hasta que número desea contar")), l = [];
+    for (let i = 1; i <= n; i++) {
         l.push(i)
     }
+    document.getElementById("contadorN").innerHTML = `<p>El conteo a terminado y fue:<br>${l}</p>`
+}
+
+function suma(){
+    let n = [], i = 1, resultado = 0;
+    while (i <= 5) {
+        n.push(parseInt(prompt("ingrese un numero para despues sumar")))
+        i++
+    }
+    i = 0
+    while (i < 5){
+        resultado = resultado + n[i]
+        i++
+    }
+    document.getElementById("resultadoSuma").innerHTML = `<p>El resultado final de la suma de ${n.join(" + ")}es:<br>${resultado}</p>`
 }
 
 
